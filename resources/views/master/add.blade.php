78b8858fe75e060683a92a1b83339d17ad93edef
ozawa_shotaro <ozawa@ozawa-shotaronoMacBook-Pro.local>
2020-01-23 21:59:50 +0900

    マスター関連のViewのテンプレート化(継承機能実装)

--- resources/views/master/add.blade.php
+++ resources/views/master/add.blade.php
@@ -1,31 +1,27 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>ユーザー追加</title>
-</head>
-<body>
+@extends('layouts.base')
 
-	<form action="{{ route('user.add') }}" method="get">
-		<ul>
-			<li>
-				名前：<input type="text" name="name" required>
-			</li>
-			<li>
-				メールアドレス：<input type="text" name="email" required>
-			</li>
-			<li>
-				<select name="auth">
-					<option value="0">幹部以上</option>
-					<option value="1">一般</option>
-				</select>
-			</li>
-		</ul>
+@section('title')
+ユーザー追加
+@endsection
 
-		<button type="submit">ユーザー追加</button>
-	</form>
+@section('body')
+<form action="{{ route('user.add') }}" method="get">
+	@csrf
+	<ul>
+		<li>
+			名前：<input type="text" name="name" required>
+		</li>
+		<li>
+			メールアドレス：<input type="text" name="email" required>
+		</li>
+		<li>
+			<select name="auth">
+				<option value="0">幹部以上</option>
+				<option value="1">一般</option>
+			</select>
+		</li>
+	</ul>
 
-</body>
-</html>
+	<button type="submit">ユーザー追加</button>
+</form>
+@endsection
