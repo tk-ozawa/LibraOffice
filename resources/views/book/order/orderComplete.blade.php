df4abf49182c00b09fc6aa17f5e785f174511a70
ozawa_shotaro <ozawa@ozawa-shotaronoMacBook-Pro.local>
2020-01-23 21:54:10 +0900

    書籍関係のViewのテンプレート化(継承機能実装)

--- resources/views/book/order/orderComplete.blade.php
+++ resources/views/book/order/orderComplete.blade.php
@@ -1,43 +1,46 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>書籍注文依頼完了画面</title>
-</head>
-<body>
-	<h2>注文依頼を送信しました。</h2>
+@extends('layouts.order')
 
-	タイトル:{{ $bookProp->title }}
-	<br>
-	ISBN:@if(empty($bookProp->ISBN)) なし @else {{ $bookProp->ISBN }} @endif
-	<br>
-	版数:@if(empty($bookProp->edition)) なし @else {{ $bookProp->edition }} @endif
-	<br>
-	価格:{{ $bookProp->price }}
-	<br>
-	発売日:{{ $bookProp->release_date }}
-	<br>
-	カテゴリ:
-	@foreach ($bookProp->categories as $category)
-		@if (!$loop->first),@endif
-		{{ $category->name }}
-	@endforeach
-	<br>
-	著者:
-	@foreach ($bookProp->authors as $author)
-		@if (!$loop->first),@endif
-		{{ $author->name }}
-	@endforeach
-	<br>
-	出版元:{{ $publisherProp->name }}
+@section('title')
+書籍注文依頼完了
+@endsection
 
-	<br>
-	@if (session('auth') === 0)
-		<a href="{{ route('master.top') }}">戻る</a>
-	@else
-		<a href="{{ route('normal.top') }}">戻る</a>
-	@endif
-</body>
-</html>
+@section('navLinkHref')
+href="{{ route('search') }}"
+@endsection
+
+@section('mainBody')
+
+<h2>注文依頼を送信しました。</h2>
+
+タイトル:{{ $bookProp->title }}
+<br>
+ISBN:@if(empty($bookProp->ISBN)) なし @else {{ $bookProp->ISBN }} @endif
+<br>
+版数:@if(empty($bookProp->edition)) なし @else {{ $bookProp->edition }} @endif
+<br>
+価格:{{ $bookProp->price }}
+<br>
+発売日:{{ $bookProp->release_date }}
+<br>
+カテゴリ:
+@foreach ($bookProp->categories as $category)
+	@if (!$loop->first),@endif
+	{{ $category->name }}
+@endforeach
+<br>
+著者:
+@foreach ($bookProp->authors as $author)
+	@if (!$loop->first),@endif
+	{{ $author->name }}
+@endforeach
+<br>
+出版元:{{ $publisherProp->name }}
+
+<br>
+@if (session('auth') === 0)
+	<a href="{{ route('master.top') }}">戻る</a>
+@else
+	<a href="{{ route('normal.top') }}">戻る</a>
+@endif
+
+@endsection
