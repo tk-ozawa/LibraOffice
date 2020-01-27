3d4107f7825e3e6682c20660792619f20a120527
ozawa_shotaro <39585292+tk-ozawa@users.noreply.github.com>
2020-01-27 19:44:08 +0900

    Revert "fix: ログイン画面でBootstrapを使うようにした。パスワード表示機能実装"

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,81 +1,23 @@
-@extends('layouts.loginBase')
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>ログイン</title>
+</head>
+<body>
 
-@section('title')
-ログイン - LibraOffice
-@endsection
+	@if(session('valiMsg'))
+		<p>{{ session('valiMsg') }}</p>
+	@endif
 
-@section('body')
-@if(session('valiMsg'))
-<div class="alert alert-warning" role="alert">
-	<span>{{ session('valiMsg') }}</span>
-</div>
-@endif
-@if(session('flashMsg'))
-<div class="alert alert-success" role="alert">
-	<span>{{ session('flashMsg') }}</span>
-</div>
-@endif
-
-<main class="login-form">
-	<div class="cotainer">
-		<div class="row justify-content-center">
-			<div class="col-md-8">
-				<div class="card">
-					<div class="card-header">ログイン</div>
-					<div class="card-body">
-						<form action="{{ route('login') }}" method="POST">
-							@csrf
-							<div class="form-group row">
-								<label for="email_address" class="col-md-4 col-form-label text-md-right">メールアドレス</label>
-								<div class="col-md-6">
-									<input type="text" id="email_address" class="form-control" name="email" required autofocus>
-								</div>
-							</div>
-
-							<div class="form-group row">
-								<label for="password" class="col-md-4 col-form-label text-md-right">パスワード</label>
-								<div class="col-md-6">
-									<input type="password" id="password" class="form-control" name="password" required>
-								</div>
-							</div>
-
-							<div class="form-group row">
-								<div class="col-md-6 offset-md-4">
-									<div class="checkbox">
-										<label>
-											<input type="checkbox" id="password-display">パスワードを表示
-										</label>
-									</div>
-								</div>
-							</div>
-
-							<div class="col-md-6 offset-md-4">
-								<button type="submit" class="btn btn-primary">
-									ログイン
-								</button>
-								{{-- <a href="#" class="btn btn-link">
-									Forgot Your Password?
-								</a> --}}
-							</div>
-						</form>
-					</div>
-				</div>
-			</div>
-		</div>
-	</div>
-	</div>
-
-</main>
-
-<script>
-	const pwd = document.getElementById('password');
-	const pwdCheck = document.getElementById('password-display');
-	pwdCheck.addEventListener('change', function() {
-		if(pwdCheck.checked) {
-			pwd.setAttribute('type', 'text');
-		} else {
-			pwd.setAttribute('type', 'password');
-		}
-	}, false);
-</script>
-@endsection
+	<form action="login" method="get">
+		メールアドレス：<input type="text" name="email" required>
+		<br>
+		パスワード<input type="text" name="password" required>
+		<br>
+		<button type="submit">ログイン</button>
+	</form>
+</body>
+</html>
