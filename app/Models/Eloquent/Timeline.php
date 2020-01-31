2048c15963e95289d0b11b4110ffa6c1f465c746
ozawa_shotaro <ozawa@ozawa-shotaronoMacBook-Pro.local>
2020-01-31 12:05:40 +0900

    create: migrationとmodel作成

---
+++ app/Models/Eloquent/Timeline.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Eloquent;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Timeline extends Model
+{
+	protected $table = 'timeline';
+
+	public $timestamps = true;	// created_at, updated_at有
+
+	protected $fillable = ['content', 'user_id', 'purchase_id'];
+
+	public function purchases()
+	{
+		// 多対1
+		return $this->belongsTo(
+			'App\Models\Eloquent\Purchase'			// 親テーブルモデル
+			, 'purchase_id'					// 参照元の外部キー
+			, 'id'							// 参照先の主キー
+		);
+	}
+
+	public function users()	// 要確認:リレーション関係の正確性
+	{
+		// 多対1
+		return $this->belongsTo(
+			'App\Models\Eloquent\User'				// 親テーブルモデル
+			, 'user_id'						// 参照元の外部キー
+			, 'id'							// 参照先の主キー
+		);
+	}
+}
--- database/migrations/2020_01_31_024805_create_timeline_table.php
+++ database/migrations/2020_01_31_024805_create_timeline_table.php
@@ -6,26 +6,32 @@ use Illuminate\Support\Facades\Schema;
 
 class CreateTimelineTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('timeline', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->timestamps();
-        });
-    }
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('timeline', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('content')->comment('行った処理の内容');
+			$table->integer('user_id')->comment('ユーザーID')->unsigned();
+			$table->integer('purchase_id')->comment('所持書籍ID')->unsigned();
+			$table->timestamps();
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::dropIfExists('timeline');
-    }
+			$table->foreign('user_id')->references('id')->on('users');
+			$table->foreign('purchase_id')->references('id')->on('purchases');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('timeline');
+	}
 }
