5edcae85279ca62d220017866186d00f9cbfacd4
ozawa_shotaro <ozawa@ozawa-shotaronoMacBook-Pro.local>
2020-02-04 13:44:23 +0900

    create: テーブル作成

---
+++ database/migrations/2020_02_04_133748_create_favorites_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateFavoritesTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('favorites', function (Blueprint $table) {
+			$table->increments('id');
+			$table->integer('timeline_id')->commnet('タイムラインID')->unsigned();
+			$table->integer('user_id')->commnet('お気に入りしたユーザーID')->unsigned();
+			$table->integer('status')->default(1)->commnet('0:無効, 1:有効');
+
+			$table->foreign('timeline_id')->references('id')->on('timeline');
+			$table->foreign('user_id')->references('id')->on('users');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::dropIfExists('favorites');
+	}
+}
