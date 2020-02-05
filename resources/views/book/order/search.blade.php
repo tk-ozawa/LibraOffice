195f420311111f23691e8b8a903b4d7985606988
ozawa_shotaro <ozawa@ozawa-shotaronoMacBook-Pro.local>
2020-02-05 15:40:36 +0900

    fix: 注文系画面でBootstrap4を利用していなかった為修正

--- resources/views/book/order/search.blade.php
+++ resources/views/book/order/search.blade.php
@@ -5,12 +5,21 @@
 @endsection
 
 @section('mainBody')
+<div class="container">
 <form action="{{ route('search.order.input')}}" method="GET">
 	@csrf
-	ISBN<input type="text" name="ISBN" required>
-	<br>
-	第n版<input type="number" min="1" step="1" name="edition" value="1" required>
-	<br>
-	<button type="submit">検索</button>
+	<div class="form-group">
+		ISBN
+		<input class="form-control" type="text" name="ISBN" required>
+	</div>
+	<div class="form-group">
+		第n版
+		<input class="form-control" type="number" min="1" step="1" name="edition" value="1" required>
+	</div>
+	<button type="submit" class="btn btn-primary">検索</button>
 </form>
+<br>
+<p>手動入力フォームは <a href="/order/input">こちら</a></p>
+</div>
+
 @endsection
