70b44deaefe4bc9d75c7e647e30fc3fc9ea3fb4c
ozawa_shotaro <ozawa@ozawa-shotaronoMacBook-Pro.local>
2020-02-14 14:17:46 +0900

    update: ユーザー追加画面作成

--- resources/views/user/add.blade.php
+++ resources/views/user/add.blade.php
@@ -1,27 +1,26 @@
-@extends('layouts.base')
+@extends('layouts.userList')
 
 @section('title')
 ユーザー追加フォーム
 @endsection
 
 @section('body')
-<form action="{{ route('user.add') }}" method="get">
+<form action="{{ route('user.add') }}" method="POST">
 	@csrf
-	<ul>
-		<li>
-			名前：<input type="text" name="name" required>
-		</li>
-		<li>
-			メールアドレス：<input type="text" name="email" required>
-		</li>
-		<li>
-			<select name="auth">
-				<option value="0">幹部以上</option>
-				<option value="1">一般</option>
-			</select>
-		</li>
-	</ul>
-
-	<button type="submit">ユーザー追加</button>
+	<div class="form-group">
+		名前
+		<input class="form-control" type="text" name="name" required>
+	</div>
+	<div class="form-group">
+		メールアドレス
+		<input class="form-control" type="text" name="email" required>
+	</div>
+	<div class="form-group">
+		<select class="form-control" name="auth">
+			<option value="0">幹部以上</option>
+			<option value="1">一般</option>
+		</select>
+	</div>
+	<button class="btn btn-success" type="submit">この内容でユーザーを追加する</button>
 </form>
 @endsection
