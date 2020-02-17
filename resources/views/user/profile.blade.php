af0be7c5aa3d1d7091e50ad39adcd8c6c56242dd
ozawa_shotaro <ozawa@ozawa-shotaronoMacBook-Pro.local>
2020-02-17 22:17:14 +0900

    fix: 貸出/返却ボタンの排他制御を修正

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -25,7 +25,7 @@
 <h3>{{ $user->name }}さんが借りたことのある書籍</h3>
 
 @if($rentals)
-<p>{{ $rentals->count() }}件ヒットしました</p>
+<p>{{ count($rentals) }}件ヒットしました</p>
 <div class="table-responsive">
 	<table class="table text-nowrap">
 		<thead>
@@ -40,21 +40,32 @@
 		</thead>
 		<tbody>
 			@foreach($rentals as $ren)
-			@php $book = $ren->purchases->books; @endphp
+			@php $book = $ren['purchases']->books; @endphp
 			<tr>
 				<th scope="row" class="index-col">{{ $loop->iteration }}</th scope="row">
 				<td class="btn-col">
-					<button class="btn btn-danger" onclick="ReturnCheck({{ $ren->purchase_id }}, '{{ $book->title }}');">返却する</button>
+
+
+					@if ($ren['isRental'])
+						@if ($ren['rentalUserId'] === session('id'))
+							<button class="btn btn-danger" onclick="ReturnCheck({{ $ren['purchases']->id }}, '{{ $book->title }}');">返却する</button>
+						@else
+							<a class="btn btn-warning">貸出中</a>
+						@endif
+					@else
+						<button class="btn btn-success" onclick="RentalCheck({{ $ren['purchases']->id }}, '{{ $book->title }}');">借りて読む</button>
+					@endif
+
 				</td>
-				<td class="img-col"><a href="{{ route('book.detail', ['purchaseId' => $ren->purchase_id]) }}"><img src="{{ $book->img_url }}"></a></td>
-				<td><a href="{{ route('book.detail', ['purchaseId' => $ren->purchase_id]) }}">{{ $book->title }} 第{{ $book->edition }}版</a></td>
+				<td class="img-col"><a href="{{ route('book.detail', ['purchaseId' => $ren['purchases']->id]) }}"><img src="{{ $book->img_url }}"></a></td>
+				<td><a href="{{ route('book.detail', ['purchaseId' => $ren['purchases']->id]) }}">{{ $book->title }} 第{{ $book->edition }}版</a></td>
 				<td class="cat-col">
 					@foreach ($book->categories as $category)
 						<a href="{{ route('book.find.category', ['categoryName' => $category['name']]) }}">{{ $category['name'] }}</a>
 						@if(!$loop->last),@endif
 					@endforeach
 				</td>
-				<td>{{ $ren->purchases->purchase_date }}</td>
+				<td>{{ $ren['purchases']->purchase_date }}</td>
 				</tr>
 			@if(!$loop->last) <br> @endif
 			@endforeach
